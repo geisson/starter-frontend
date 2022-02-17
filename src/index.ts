c9d1b215042e840a16801a6539c8bc077c0797ac
Geisson Iuri <geissoniuri@gmail.com>
2022-02-17 06:37:07 -0300

    teste typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 import '../sass/style.scss';
+
+const inputName = document.querySelector('[data-teste="nome"]') as HTMLInputElement;
+const btn = document.querySelector('[data-teste="btn"]') as HTMLButtonElement;
+
+const handleClick = (event:Event):void => {
+  event.preventDefault();
+
+  const inputValue = inputName.value;
+
+  console.log(`Ola mundo ${inputValue}`);
+  console.log(this);
+  console.log(event);
+};
+
+btn?.addEventListener('click', handleClick);
