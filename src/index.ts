f620e7b690bc2f2a8a8f3761c757f9cf1f65d383
Geisson Iuri <geissoniuri@gmail.com>
2022-06-01 22:55:57 -0300

    chore(build): create the distribution version of the project

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
 import '../sass/style.scss';
 import '../index.html';
 
-const inputName = document.querySelector(
-  '[data-teste="nome"]',
-) as HTMLInputElement;
-const btn = document.querySelector('[data-teste="btn"]') as HTMLButtonElement;
-
-const handleClick = (event: Event): void => {
-  event.preventDefault();
-
-  const inputValue = inputName.value;
-
-  console.log(`Ola mundo ${inputValue}!!!!!`);
-};
-
-btn.addEventListener('click', handleClick);
+console.log('ola mundo!');
